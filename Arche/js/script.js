fb1c9747a0b6d03e27ffee102990f80b57a1cea2
kazisolemans <kazisoleman@gmail.com>
2020-03-15 23:14:36 +0600

    This is Arche

---
+++ Arche/js/script.js
@@ -0,0 +1,43 @@
+
+$('.banner_slide').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+});
+
+
+
+//$('.service_slide').slick({
+//  infinite: true,
+//  slidesToShow: 2,
+//  slidesToScroll: 2,
+//  autoplay: true,
+//    autoplaySpeed: 1000,
+//    arrows: false,
+//});
+//
+//$('.project_slide').slick({
+//  infinite: true,
+//  slidesToShow: 4,
+//  slidesToScroll: 1,
+//  autoplay: false,
+//    autoplaySpeed: 1000,
+//    arrows: false,
+//});
+//
+//$('.testimonial_image').slick({
+//  infinite: true,
+//  slidesToShow: 2,
+//  slidesToScroll: 1,
+//  autoplay: false,
+//    autoplaySpeed: 1000,
+//    arrows: false,
+//});
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
